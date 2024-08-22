0299be1acd61cd69ea628b936f917c506c9976e3
Queñano Jhon Marcellino <okayda999@gmail.com>
2024-08-22 02:39:24 -1200

    Add minor adjustment at GradientText pill

--- components/animation/GradientText.tsx
+++ components/animation/GradientText.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function GradientText({ text = "Your text" }: { text: string }) {
   return (
     <div>
-      <div className="group relative mx-auto flex max-w-fit flex-row items-center justify-center rounded-2xl bg-[#03001417] px-4 py-1.5 font-medium shadow-[inset_0_-8px_10px_#8fdfff1f] backdrop-blur-sm transition-shadow duration-500 ease-out [--bg-size:300%] hover:shadow-[inset_0_-5px_10px_#8fdfff3f]">
+      <div className="group relative mx-auto flex max-w-fit flex-row items-center justify-center rounded-2xl bg-[#03001417] px-3 py-1 font-medium shadow-[inset_0_-8px_10px_#8fdfff1f] backdrop-blur-sm transition-shadow duration-500 ease-out [--bg-size:300%] hover:shadow-[inset_0_-5px_10px_#8fdfff3f]">
         <div
           className={`absolute inset-0 block h-full w-full animate-gradient bg-gradient-to-r from-[#FFAA40]/50 via-[#FFF]/50 to-[#FFAA40]/50 bg-[length:var(--bg-size)_100%] p-[1px] [border-radius:inherit] ![mask-composite:subtract] [mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)]`}
         />
